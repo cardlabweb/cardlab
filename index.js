c5e3e5c19b04f6ea7fc2f2ba70679036f69a5ec3
armbusines <grigoryanzaqar1@gmail.com>
2026-01-16 17:47:05 +0400

    Refactor DOMContentLoaded event listener setup

--- index.js
+++ index.js
@@ -20,29 +20,5 @@ const db = firebase.database();
       Գին՝ ${ad.price}
     `;
 
-    container.appendChild(div);
-  });
-});
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC3GWm__vMzHBH01yBH_nOLO-dLrQ9we3g",
-  authDomain: "cardlabarmenian.firebaseapp.com",
-  projectId: "cardlabarmenian",
-  storageBucket: "cardlabarmenian.firebasestorage.app",
-  messagingSenderId: "226139663046",
-  appId: "1:226139663046:web:4353e70d03272f6e40c5c7",
-  measurementId: "G-1HCB71GJ44"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 
